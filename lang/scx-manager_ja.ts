01805c3ad85cfaa4bee57ff86ec943b0bcef1b03
MintJapan <103297262+MintJapan@users.noreply.github.com>
2026-03-13 21:09:53 +0900

    i18n: add Japanese translation

---
+++ lang/scx-manager_ja.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>SchedExtWindow</name>
+    <message>
+        <location filename="../src/schedext-window.ui" line="17"/>
+        <source>CachyOS Configure sched-ext</source>
+        <translation>CachyOS sched-ext 設定</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="40"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Configure sched-ext scheduler:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;sched-ext スケジューラ設定&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="75"/>
+        <source>Running sched-ext scheduler:</source>
+        <translation>実行中の sched-ext スケジューラ:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="82"/>
+        <source>Select sched-ext scheduler:</source>
+        <translation>実行するスケジューラ:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="92"/>
+        <source>Select scheduler profile:</source>
+        <translation>スケジューラプロファイル:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="115"/>
+        <source>unknown</source>
+        <translation>不明</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="122"/>
+        <source>Set sched-ext extra scheduler flags:</source>
+        <translation>sched-ext 追加フラグ:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="176"/>
+        <source>Disable</source>
+        <translation>無効化</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="183"/>
+        <source>Apply</source>
+        <translation>適用</translation>
+    </message>
+</context>
+<context>
+    <name>scxctl::impl::SchedExtWindow</name>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="122"/>
+        <source>Cannot initialize scx_loader configuration</source>
+        <translation>scx_loader の設定を初期化できません</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="138"/>
+        <source>Cannot get information from scx_loader!
+Is it working?
+This is needed for the app to work properly</source>
+        <translation>scx_loader から情報を取得できません。
+scx_loader が動作していることを確認してください。
+アプリが正常に動作するために必要です</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="205"/>
+        <source>Cannot disable scx_loader</source>
+        <translation>scx_loader を無効化できません</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="223"/>
+        <source>Cannot get scx flags from scx_loader configuration!</source>
+        <translation>scx_loader の設定から scx フラグを取得できません。</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="259"/>
+        <source>Cannot set default scx scheduler with mode! Scheduler %1 with mode %2</source>
+        <translation>scx スケジューラ %1 を %2 モードでデフォルトに設定することはできません</translation>
+    </message>
+</context>
+</TS>
